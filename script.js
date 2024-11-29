85f8e1b8bc545a68f9525994b3f7a498daaff555
Abhishek Raghav <115450330+abhiraghav23@users.noreply.github.com>
2024-11-29 14:27:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-//your JS code here. If required.
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+  event.preventDefault();
+const firstName = document.getElementById('firstname').value;
+const lastName = document.getElementById('lastname').value;
+const Phone = document.getElementById('phone').value;
+const Email = document.getElementById('email').value;
+
+alert(`First Name: ${firstName}  Last Name: ${lastName}  Phone Number: ${Phone}  Email ID: ${Email}` );
+});
+	
