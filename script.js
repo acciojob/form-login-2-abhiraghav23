ebc90d401eecced3f9bef08b4080b9b9f1f33580
Abhishek Raghav <115450330+abhiraghav23@users.noreply.github.com>
2024-12-01 19:59:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,6 +5,6 @@ const lastName = document.getElementById('lastname').value;
 const Phone = document.getElementById('phone').value;
 const Email = document.getElementById('email').value;
 
-alert(`First Name: ${firstName}  Last Name: ${lastName}  Phone Number: ${Phone}  Email ID: ${Email}` );
+alert(`First Name: ${firstName} Last Name: ${lastName} Phone Number: ${Phone} Email ID: ${Email}` );
 });
 	
